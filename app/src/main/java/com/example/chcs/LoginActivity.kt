b5263ab3f48cab50a5279e9f34538c507d46d28a
Janette <janetifeoluwa09@gmail.com>
2024-01-06 03:10:49 +0100

    New updates

--- app/src/main/java/com/example/chcs/LoginActivity.kt
+++ app/src/main/java/com/example/chcs/LoginActivity.kt
@@ -2,10 +2,29 @@ package com.example.chcs
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.content.Intent
+
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val forgotPasswordBtn: Button = findViewById(R.id.forgotPassword)
+
+        forgotPasswordBtn.setOnClickListener{
+            val intent= Intent(this, ForgotPasswordActivity::class.java)
+
+            startActivity(intent)
+        }
+
+        val createNewBtn: Button = findViewById(R.id.createNewAcct)
+
+        createNewBtn.setOnClickListener {
+            val i = Intent(this, CreateNewAccountActivity::class.java)
+
+            startActivity(i)
+        }
     }
 }
--- app/src/main/java/com/example/chcs/MainActivity.kt
+++ app/src/main/java/com/example/chcs/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.chcs
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.filament.View
+import kotlin.reflect.KClass
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.joinButton)
+
+        button.setOnClickListener {
+            val intent= Intent(this, LoginActivity::class.java)
+
+            startActivity(intent)
+        }
+
     }
 }
